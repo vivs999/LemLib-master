09c88d67f88c00071577266a4878da016daab75a
vivs999 <rochlanivivaan@gmail.com>
2024-12-13 22:36:41 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {}
+
+/**
+ * Runs while the robot is disabled
+ */
+void disabled() {}
+
+/**
+ * runs after initialize if the robot is connected to field control
+ */
+void competition_initialize() {}
+
+/**
+ * Runs during auto
+ *
+ * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
+ */
+void autonomous() {}
+
+/**
+ * Runs in driver control
+ */
+void opcontrol() {}
